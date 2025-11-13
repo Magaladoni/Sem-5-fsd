db37445fb6bb1af6bf00e38450f93c97013bd58a
Mayank jena <mayankjena396@gmail.com>
2025-11-13 14:45:26 +0530

    day5

---
+++ day 5/index.js
@@ -0,0 +1,5 @@
+console.log("Welcome to fsd session");
+document.write("Welcome to fsd session");
+document.writeln("Welcome to fsd session");
+document.writeln("Welcome to fsd session");
+alert("alert message")
